dc16a7a934bafa1d3f0ad1aab7a41ba9017aa094
Dillon <dmanhall@gmail.com>
2018-12-04 13:09:53 -0700

    Added LeNet to Le net file.

--- LeNet.py
+++ LeNet.py
@@ -6,3 +6,35 @@ from keras.models import load_model # added
 import matplotlib.pyplot as plt
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from keras.regularizers import l2
+
+# lenet, from Chao's minst hand writing exsample 
+def LeNet(width=1, height=1, depth=1, classes=1):
+		# initialize the model
+		model = Sequential()
+		inputShape = (height, width, depth)
+
+		# if we are using "channels first", update the input shape
+		#if K.image_data_format() == "channels_first":
+		#	inputShape = (depth, height, width)
+
+		# first set of CONV => RELU => POOL layers
+		model.add(Conv2D(20, (5, 5), padding="same", input_shape=inputShape))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+		# second set of CONV => RELU => POOL layers
+		model.add(Conv2D(50, (5, 5), padding="same"))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+		# first (and only) set of FC => RELU layers
+		model.add(Flatten())
+		model.add(Dense(500))
+		model.add(Activation("relu"))
+
+		# softmax classifier
+		model.add(Dense(classes))
+		model.add(Activation("softmax"))
+
+		# return the constructed network architecture
+		return model
